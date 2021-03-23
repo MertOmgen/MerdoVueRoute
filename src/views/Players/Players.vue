2b4936c9f6d88bec2b0b36ccb240d56f188cd3f6
Mert Ömgen <mert.omgen@gratas.com.tr>
2021-03-23 18:39:25 +0300

    Data Fetching

--- src/views/Players/Players.vue
+++ src/views/Players/Players.vue
@@ -2,12 +2,14 @@
   <h1>Players</h1>
   <div v-for="player in players" :key="player.id">
       <router-link :to="{name:'Players Details',params:{ id:player.id }}">
+
           <!-- router-link icerisinde bulunan 'params' propertysi ile bu linke tıklandıgında gonderilecek propertylerimizi tanımlıyoruz
           Burada bir key-value pair duzeni bulunmakta. Her key router parametresini temsil eder
           Key; Gonderecegimiz parametre adi('id')
           Value; Parametre ile tasinacak veri ('player.id') 
             -->
-      <h2>{{player.title}}</h2>
+      <h2>{{player.Name}}</h2>
+
       </router-link> 
   </div>
 </template>
@@ -16,13 +18,21 @@
 export default {
 data(){
     return {
-        players:[
-            {title: 'Alex de Souza',id:10,details:'MO'},
-            {title: 'Moussa Sow',id:7,details:'ST'},
-            {title: 'Dirk Kuyt',id:11,details:'RW'},
+        // players:[
+        //     {title: 'Alex de Souza',id:10,details:'MO'},
+        //     {title: 'Moussa Sow',id:7,details:'ST'},
+        //     {title: 'Dirk Kuyt',id:11,details:'RW'},
 
-        ]
+        // ]
+
+        players:[]
     }
+},
+mounted(){
+     fetch('http://localhost:3000/players')
+  .then(res=>res.json())
+  .then(data=>this.players = data)
+  .catch(err=> console.error(err.message))
 }
 }
 </script>
--- src/views/Players/PlayersDetails.vue
+++ src/views/Players/PlayersDetails.vue
@@ -1,16 +1,30 @@
 <template>
   <h1 style="background:darkblue;color:yellow">EN BUYUK FENERBAHCE</h1>
+  <div v-if="player">
+ <p> {{player.Name}} </p>
   <p> {{id}} </p>
+  <p> {{player.Position}} </p>
+  </div>
+ 
 </template>
 
 <script>
 export default {
+   props:['id'],
+
 data(){
-  props:['id']
   return{
         // id: this.$route.params.id
+
+        player:null
         
     }
+},
+mounted(){
+  fetch('http://localhost:3000/players/' + this.id)
+  .then(res=>res.json())
+  .then(data=>this.player = data)
+  .catch(err=> console.error(err.message))
 }
 }
 </script>
